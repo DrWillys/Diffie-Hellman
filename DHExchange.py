373b944aa0430d03e64b161d104b07fbf7e8dc1a
Erik Setterquist <eriksetterquist@gmail.com>
2017-09-16 04:04:25 +0200

    Initial project version

---
+++ DHExchange.py
@@ -0,0 +1,27 @@
+#imports
+
+#local imports
+
+class DHExchange:
+
+    def __init__(self ):
+        """
+        It would be interesting to try the hack of precomputing the first three steps 
+        for a very small p. Just to learn how to.
+        """
+        
+    def primRootModulo(self, p):
+        """
+
+        """
+
+    def coprime(self, p):
+        """
+
+        """
+
+    def isCongruent(self, p):
+        """
+
+        """
+        
